132fb4e36887c877bb9a9c57105973da6623a77c
Victoria Petkova <98077277+Viktoria181@users.noreply.github.com>
2024-09-29 01:32:05 +0300

    Create Задача_02.cpp

---
+++ Домашно_01/Задача_02.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    // Деклариране на променливите
+    float weightInKg;         // маса в килограми
+    float heightInMeters;     // ръст в метри
+    float bodyMassIndex;      // индекс на телесната маса (BMI)
+
+    // Въвеждане на масата и ръста
+    std::cout << "Въведете тегло в килограми: ";
+    std::cin >> weightInKg;
+
+    std::cout << "Въведете ръст в метри: ";
+    std::cin >> heightInMeters;
+
+    // Изчисляване на индекса на телесната маса
+    bodyMassIndex = weightInKg / (heightInMeters * heightInMeters);
+
+    // Извеждане на резултата
+    std::cout << "Индексът на телесната маса (BMI) е: " << bodyMassIndex << std::endl;
+
+    return 0;
+}
