52a73a5a031fe8a83675238b47b229afef1e9f61
Victoria Petkova <98077277+Viktoria181@users.noreply.github.com>
2024-09-29 23:26:19 +0300

    Update program.cpp

--- Домашно_01/Exercise_06/program.cpp
+++ Домашно_01/Exercise_06/program.cpp
@@ -4,17 +4,17 @@
 int main() {
     int num1, num2;
 
-    std::cout << "Enter first number : ";
+    std::cout << "Enter first number: ";
     std::cin >> num1;
 
-    std::cout << "Enter second number : ";
+    std::cout << "Enter second number: ";
     std::cin >> num2;
 
-    std::cout << "Before swap : Number1 = " << num1 << ", Number2 = " << num2 << std::endl;
+    std::cout << "Before swap: Number1 = " << num1 << ", Number2 = " << num2 << std::endl;
 
     swap(num1, num2);
 
-    std::cout << "After swap : Number1 = " << num1 << ", Number2 = " << num2 << std::endl;
+    std::cout << "After swap: Number1 = " << num1 << ", Number2 = " << num2 << std::endl;
 
     return 0;
 }
