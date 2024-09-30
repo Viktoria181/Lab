775a4388f1b145495ccc45cfaffd1f078d982c9f
Victoria Petkova <98077277+Viktoria181@users.noreply.github.com>
2024-09-30 14:50:28 +0300

    Add files via upload

---
+++ Exercises/ExercisesStrypesLabPart1.cpp
@@ -0,0 +1,20 @@
+﻿/*
+Описание: Напишете програма, която чете две цели числа от стандартния вход (cin) и 
+отпечатва тяхната сума на стандартния изход (cout).
+*/
+
+#include <iostream>
+
+int main()
+{
+	int number1 = 0;
+	int number2 = 0;
+
+	std::cout << "Enter the first number: ";
+	std::cin >> number1;
+	std::cout << "Enter the second number: " << number2;
+	std::cin >> number2;
+
+	std::cout << "The first number is: " << number1 << ", \nthe second one is " << number2;
+
+}
