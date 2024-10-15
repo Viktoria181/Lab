080c02dc98168ee66f4089391573ce7c5d68bd81
Victoria Petkova <98077277+Viktoria181@users.noreply.github.com>
2024-10-15 23:56:51 +0300

    Delete Homework_04 directory

--- Homework_04/Exercise_01/Attendance.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <cstdint> // Include for uint64_t
-
-void setAttendance(uint64_t& attendance, int student);
-void clearAttendance(uint64_t& attendance, int student);
-void printAbsent(uint64_t attendance);
-void printPresent(uint64_t attendance);
-void toggleAttendance(uint64_t& attendance, int student);
--- Homework_04/Exercise_02/circle.h
+++
@@ -1,8 +0,0 @@
-#ifndef CIRCLE_H  // preventing multiple inclusion
-#define CIRCLE_H
-
-const double PI = 3.14; // Constant for Pi value
-
-int circle(double radius, double* perimeter, double* area);
-
-#endif  // CIRCLE_H
