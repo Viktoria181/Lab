bdae6b090e7d7f4b475321cb412eeee5754ed557
Victoria Petkova <98077277+Viktoria181@users.noreply.github.com>
2024-10-16 22:40:07 +0300

    Update calculator.cpp

--- Homework_03/Exercise_03/calculator.cpp
+++ Homework_03/Exercise_03/calculator.cpp
@@ -1,36 +1,91 @@
-﻿#include <iostream>
-#include <iomanip>  
+#include <iostream>
+#include <iomanip>  // За std::setprecision
 
-int main() {
-    double number1, number2, result;
-    char operation;
+double stringToDouble(const char* str) {
+    
+    double result = 0.0;
+    int sign = 1;
+    bool decimalFound = false;
+    double decimalMultiplier = 0.1;
+    int i = 0;
 
-    std::cout << "Enter an expression (e.g., 10 + 5.2): ";
-    std::cin >> number1 >> operation >> number2;
+    // Проверяваме за отрицателен знак
+    if (str[i] == '-') {
+        sign = -1;
+        i++;
+    }
 
-    switch (operation) {
-    case '+':
-        result = number1 + number2;
-        break;
-    case '-':
-        result = number1 - number2;
-        break;
-    case 'x':
-        result = number1 * number2;
-        break;
-    case '/':
-        if (number2 == 0) {
-            std::cerr << "Error! Division by zero!" << std::endl;
-            return 1;
+    // Обработваме числата
+    for (; str[i] != '\0'; ++i) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            if (decimalFound) {
+                // След десетичната запетая, умножаваме всяка цифра 
+                // по съответната десетична стойност
+                result += (str[i] - '0') * decimalMultiplier;
+                decimalMultiplier /= 10;
+            }
+            else {
+                // Преди десетичната запетая, преместваме цифрите наляво 
+                // (умножаваме по 10) и добавяме новата цифра
+                result = result * 10 + (str[i] - '0');
+            }
         }
-        result = number1 / number2;
-        break;
-    default:
-        std::cerr << "Error! Invalid operation!" << std::endl;
+        else if (str[i] == '.') {
+            if (decimalFound) {
+                // Ако е намерена втора десетична запетая, връщаме грешка
+                std::cerr << "Error: Multiple decimal points found in number." << std::endl;
+                return 0.0;
+            }
+            decimalFound = true; // Отбелязваме, че сме срещнали десетична запетая
+        }
+        else {
+            // Невалиден символ
+            std::cerr << "Error: Invalid character in input." << std::endl;
+            return 0.0;
+        }
+    }
+
+    return result * sign;
+}
+
+int main(int argc, char* argv[]) {
+
+    // Проверка за броя на аргументите
+    if (argc != 4) {
+        std::cerr << "Error: Invalid number of arguments." << std::endl;
         return 1;
     }
 
-    std::cout << "Result is " << std::fixed << std::setprecision(2) << result << std::endl;
+    // Прочитане на числата и операцията от командния ред
+    double num1 = stringToDouble(argv[1]);
+    double num2 = stringToDouble(argv[3]);
+    char operation = argv[2][0];
+    double result;
+
+    switch (operation) {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case 'x':
+        result = num1 * num2;
+        break;
+    case '/':
+        if (num2 == 0) {
+            std::cerr << "Error: Division by zero." << std::endl;
+            return 1;
+        }
+        result = num1 / num2;
+        break;
+    default:
+        std::cerr << "Error: Invalid operation." << std::endl;
+        return 1;
+    }
+
+    // Извеждаме на резултата, закръглен до втория знак след десетичната точка
+    std::cout << "Result = " << std::fixed << std::setprecision(2) << result << std::endl;
 
     return 0;
 }
