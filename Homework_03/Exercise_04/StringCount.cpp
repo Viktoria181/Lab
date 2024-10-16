d5f223ed6c7e7025a471e260ef00590bf6134716
Victoria Petkova <98077277+Viktoria181@users.noreply.github.com>
2024-10-16 23:20:10 +0300

    Update StringCount.cpp

--- Homework_03/Exercise_04/StringCount.cpp
+++ Homework_03/Exercise_04/StringCount.cpp
@@ -1,19 +1,19 @@
 ﻿#include <iostream>
-#include <cstring>  // За strcmp
+#include <cstring>  // strcmp
 
 int main(int argc, char* argv[]) {
     // Проверяваме дали има точно един аргумент на командния ред
     if (argc != 2) {
-        std::cerr << "Usage: ./stringCount <string>\n";
+        std::cerr << "Error!\n";
         return 1;
     }
 
     // Аргументът от командния ред - търсеният стринг
     char* targetStr = argv[1];
-    char inputStr[1024];  // Можем да използваме фиксиран масив за входните данни
+    char inputStr[1024];  // Използваме фиксиран масив за входните данни
     int count = 0;
 
-    // Четем редове от стандартния вход до EOF (Ctrl+D)
+    // Четем редове от стандартния вход до EOF 
     while (std::cin.getline(inputStr, 256)) {
         // Сравняваме входния стринг с търсения стринг
         if (strcmp(inputStr, targetStr) == 0) {
