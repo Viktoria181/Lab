ae07087566fbc8b5f0829a5dde7e12663b935438
Victoria Petkova <98077277+Viktoria181@users.noreply.github.com>
2024-10-16 23:57:21 +0300

    Update Attendance.h

--- Homework_03/Exercise_01/Attendance.h
+++ Homework_03/Exercise_01/Attendance.h
@@ -1,4 +1,7 @@
-#pragma once
+#pragma once  
+#ifndef ATTENDANCE_H 
+#define ATTENDANCE_H
+
 #include <cstdint> // Include for uint64_t
 
 void setAttendance(uint64_t& attendance, int student);
@@ -6,3 +9,5 @@ void clearAttendance(uint64_t& attendance, int student);
 void printAbsent(uint64_t attendance);
 void printPresent(uint64_t attendance);
 void toggleAttendance(uint64_t& attendance, int student);
+
+#endif
